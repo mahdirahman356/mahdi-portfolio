47228ad638d58566d4393d0a769f91620033b030
kashim rahman mahdi <mahdi.rahman356@gmail.com>
2024-09-27 22:40:44 +0600

    added new project

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -5,8 +5,27 @@ const Projects = () => {
         <div className="flex justify-start items-center">
         <div className="px-5 md:px-8 lg:px-20 my-16">
             <h2 className="text-4xl font-bold ubuntu mb-16 underline">Projects</h2>
-        
-          {/* project 1 */}
+           {/* project 1 */}
+           <div className="p-8 rounded-2xl shadow-2xl mb-20">
+           <div className="mb-8">
+            <h2 className="text-3xl font-bold ubuntu mb-3 ">Step Style</h2>
+            <p className=" text-gray-500 flex items-start gap-2 mb-3">This project is a shoes ordering platform where users can order and make payments online. Admins can manage the product by adding, updating, and deleting items. and more features I have added</p>
+
+            <h2 className="text-xl font-bold ubuntu mb-3">Technology Used</h2>
+            <div className="">
+            <p className=" text-gray-500 flex items-start gap-2"><span><LuChevronRightCircle className="text-[#4D869C] mt-1 mb-2"/></span>MongoDB, Express.js, React, Node.js, Tailwind CSS, Firebase for authentication</p>
+            <p className=" text-gray-500 flex items-center gap-2"><span><LuChevronRightCircle className="text-[#4D869C] mb-2"/></span>Responsive Design</p>
+            </div>
+            </div>
+            <h2 className="text-xl font-bold ubuntu mb-3">Links</h2>
+            <div className="">
+            <a href="https://github.com/mahdirahman356/bistro-boss" className=" flex items-start gap-2 text-[#4D869C] underline"><span><LuChevronRightCircle className="text-[#4D869C] mt-1 mb-2"/></span>GitHub Code</a>
+            <a href="https://bistro-boss-2aeff.web.app/" className="flex items-center gap-2 text-[#4D869C] underline"><span><LuChevronRightCircle className="text-[#4D869C] mb-2"/></span>Live Side</a>
+            </div>
+           </div>
+
+
+          {/* project 2 */}
            <div className="p-8 rounded-2xl shadow-2xl mb-20">
            <div className="mb-8">
             <h2 className="text-3xl font-bold ubuntu mb-3 ">ElectroEvo</h2>
@@ -24,24 +43,7 @@ const Projects = () => {
             <a href="https://electroevo-89e11.web.app/" className="flex items-center gap-2 text-[#4D869C] underline"><span><LuChevronRightCircle className="text-[#4D869C] mb-2"/></span>Live Side</a>
             </div>
            </div>
-          {/* project 2 */}
-           <div className="p-8 rounded-2xl shadow-2xl mb-20">
-           <div className="mb-8">
-            <h2 className="text-3xl font-bold ubuntu mb-3 ">Bistro Boss</h2>
-            <p className=" text-gray-500 flex items-start gap-2 mb-3">This project is a food ordering platform where users can order meals and make payments online. Admins can manage the menu by adding, updating, and deleting items. and more features I have added</p>
-
-            <h2 className="text-xl font-bold ubuntu mb-3">Technology Used</h2>
-            <div className="">
-            <p className=" text-gray-500 flex items-start gap-2"><span><LuChevronRightCircle className="text-[#4D869C] mt-1 mb-2"/></span>MongoDB, Express.js, React, Node.js, Tailwind CSS, Firebase for authentication</p>
-            <p className=" text-gray-500 flex items-center gap-2"><span><LuChevronRightCircle className="text-[#4D869C] mb-2"/></span>Responsive Design</p>
-            </div>
-            </div>
-            <h2 className="text-xl font-bold ubuntu mb-3">Links</h2>
-            <div className="">
-            <a href="https://github.com/mahdirahman356/bistro-boss" className=" flex items-start gap-2 text-[#4D869C] underline"><span><LuChevronRightCircle className="text-[#4D869C] mt-1 mb-2"/></span>GitHub Code</a>
-            <a href="https://bistro-boss-2aeff.web.app/" className="flex items-center gap-2 text-[#4D869C] underline"><span><LuChevronRightCircle className="text-[#4D869C] mb-2"/></span>Live Side</a>
-            </div>
-           </div>
+          
 
            {/* project 3 */}
            <div className="p-8 rounded-2xl shadow-2xl mb-20">
